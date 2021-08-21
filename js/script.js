e8f65d5210ccc059e712dd5072fed23a9f60328f
Trauvin <mateusgalote@gmail.com>
2021-08-21 07:21:41 -0300

    finish header mobile

--- js/script.js
+++ js/script.js
@@ -30,3 +30,11 @@ function removeMenuMobile() {
 }
 
 removeMenuMobile()
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
